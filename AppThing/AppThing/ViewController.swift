c4d61eb00e6239eff0679665dc08e04300e4a5c1
Daisy Cisneros <v-dacisn@microsoft.com>
2019-07-12 11:37:27 -0700

    Adding the Swift File

---
+++ AppThing/AppThing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppThing
+//
+//  Created by Daisy Cisneros on 7/12/19.
+//  Copyright © 2019 Daisy Cisneros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
