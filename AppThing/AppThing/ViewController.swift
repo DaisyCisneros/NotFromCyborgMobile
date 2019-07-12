d7e719d211de579ffb9583b4368da0de8a8b3948
Loise Velazquez <loise.v2194@gmail.com>
2019-07-12 11:45:06 -0700

    added comment

--- AppThing/AppThing/ViewController.swift
+++ AppThing/AppThing/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// my comment - Loise Velazquez
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
